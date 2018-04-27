1ce361145e435efc874704bb05df20dab1bc1e0e
SarahRua <gaothdearg@hotmail.com>
2018-04-27 13:54:08 +0100

    Fix styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './MaterialModule';
+import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
